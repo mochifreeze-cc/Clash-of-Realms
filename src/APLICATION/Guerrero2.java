6ff913ba5624437b15b7796d7cf038e338f793c1
Helen <romcm20@gmail.com>
2025-11-19 18:36:26 -0500

    mistico y warrior2

--- src/APLICATION/Guerrero2.java
+++ src/APLICATION/Guerrero2.java
@@ -6,6 +6,11 @@ public class Guerrero2 extends Personaje {
     public Guerrero2(String nombre, int vida, int ataque, int defensa) {
         super(nombre,vida,ataque,defensa);
 
+        @Override
+        public void usarEstrategia() {
+            this.ataque = this.ataque *2;
+        }
+
     }
 
 }
--- src/APLICATION/Mistico.java
+++ src/APLICATION/Mistico.java
@@ -6,4 +6,9 @@ public class Mistico extends Personaje {
     public Mistico(String nombre, int vida, int ataque, int defensa) {
         super(nombre,vida,ataque,defensa);
     }
+
+    @Override
+    public void usarEstrategia() {
+        System.out.println("El místico usa una estrategia de magia ancestral.");
+    }
 }
