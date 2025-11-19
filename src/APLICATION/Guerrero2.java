b527c3c8ecd85d702d99b079680235c742021cde
Helen <romcm20@gmail.com>
2025-11-19 18:37:12 -0500

    ajuste

--- src/APLICATION/Guerrero2.java
+++ src/APLICATION/Guerrero2.java
@@ -6,11 +6,10 @@ public class Guerrero2 extends Personaje {
     public Guerrero2(String nombre, int vida, int ataque, int defensa) {
         super(nombre,vida,ataque,defensa);
 
-        @Override
-        public void usarEstrategia() {
-            this.ataque = this.ataque *2;
-        }
-
+    }
+    @Override
+    public void usarEstrategia() {
+        this.ataque = this.ataque *2;
     }
 
 }
